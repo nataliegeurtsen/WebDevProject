8a365f5db7bd26fa4a1b599a9a46906982faa4b8
nataliegeurtsen <94457713+nataliegeurtsen@users.noreply.github.com>
2023-03-07 13:50:44 +0100

    Homepage updated header

---
+++ WebDevProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
